a322f40a3e2248bc0281c40357b365c0dbc821ea
oyewaleoyindamola <oyewaleoyindamola>
2024-11-28 16:49:47 +0100

    get users controller updated

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,6 @@ const router = express.Router()
 router.post('/signup', signup);
 router.post('/login', login);
 router.get('/getAllUsers', getAllUsers);
-router.get('/getUsers/:role', getUsers)
+router.get('/getUsers', getUsers)
 
 module.exports = router;
