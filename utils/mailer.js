ca575c0788755d0eb9f341122db4951ef15c9390
oyewaleoyindamola <oyewaleoyindamola@gmail.com>
2025-10-13 23:35:57 +0100

    Mailer Updated

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,19 +1,13 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  secure: false,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
   },
-});
-
-transporter.verify((error, success) => {
-  if (error) {
-    console.error("❌ Email transport error:", error.message);
-  } else {
-    console.log("✅ Mailer ready to send emails");
-  }
 });
 
 module.exports = transporter;
