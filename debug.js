cc92e1a9182840b55e12f3dab75bb41aec46c601
Olawumi-Olusegun <olawumi.olusegun@gmail.com>
2026-01-20 00:11:31 +0100

    update

--- debug.js
+++
@@ -1,9 +0,0 @@
-
-import fs from 'fs';
-import util from 'util';
-
-import('./index.js').catch(err => {
-    const errorInfo = util.inspect(err, { showHidden: true, depth: null, colors: false });
-    fs.writeFileSync('debug_error.txt', errorInfo + '\n' + (err.stack || ''));
-    process.exit(1);
-});
