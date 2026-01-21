31d44bce3194060a5c70b0e394e17c4e5057a870
Owolabi shinaayomi <100287793+koredeowoblow@users.noreply.github.com>
2026-01-21 17:01:14 +0100

    Remove bulk rejection and approved applicants endpoints
    Deleted the rejectApplicationsBulk and getApprovedApplicants controller functions and their corresponding admin routes. This change removes endpoints for bulk rejecting applications and fetching approved applicants, likely for codebase simplification or feature deprecation.

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { getAllDTUsers, getAllAdminUsers, getAdminDashboard, approveAnnotator, approveUserForQA, rejectUserForQA, getAllQAUsers, rejectAnnotator, getDTUserAdmin, createAdmin, requestAdminVerification, confirmAdminVerification, verifyAdminOTP, adminLogin } = require('../controller/dtUser.controller.js');
-const { createAnnotationProject, getAllAnnotationProjects, getAnnotationProjectDetails, updateAnnotationProject, deleteAnnotationProject, requestProjectDeletionOTP, verifyOTPAndDeleteProject, getAnnotationProjectApplications, approveAnnotationProjectApplication, rejectAnnotationProjectApplication, removeApprovedApplicant, getRemovableApplicants, exportApprovedAnnotatorsCSV, attachAssessmentToProject, removeAssessmentFromProject, getAvailableAssessments, rejectApplicationsBulk, getApprovedApplicants } = require('../controller/annotationProject.controller.js');
+const { createAnnotationProject, getAllAnnotationProjects, getAnnotationProjectDetails, updateAnnotationProject, deleteAnnotationProject, requestProjectDeletionOTP, verifyOTPAndDeleteProject, getAnnotationProjectApplications, approveAnnotationProjectApplication, rejectAnnotationProjectApplication, removeApprovedApplicant, getRemovableApplicants, exportApprovedAnnotatorsCSV, attachAssessmentToProject, removeAssessmentFromProject, getAvailableAssessments} = require('../controller/annotationProject.controller.js');
 const { createInvoice, getAllInvoices, getInvoiceDetails, updatePaymentStatus, sendInvoiceReminder, deleteInvoice, bulkAuthorizePayment, generatePaystackCSV, generateMPESACSV } = require('../controller/invoice.controller.js');
 const { getAdminNotifications, createAnnouncement, getNotificationStats, cleanupNotifications, broadcastNotification } = require('../controller/notification.controller.js');
 const { getAdminAssessments, getAdminAssessmentsOverview } = require('../controller/assessment.controller.js');
@@ -41,8 +41,6 @@ router.get('/projects', authenticateAdmin, getAllAnnotationProjects);
 router.get('/projects/:projectId', authenticateAdmin, getAnnotationProjectDetails);
 router.patch('/projects/:projectId', authenticateAdmin, updateAnnotationProject);
 router.delete('/projects/:projectId', authenticateAdmin, deleteAnnotationProject);
-router.get('/projects/getApprovedApplicants/:projectId', authenticateAdmin, getApprovedApplicants);
-router.put('/projects/reject-applications-bulk',authenticateAdmin, rejectApplicationsBulk);
 // Project Deletion with OTP Routes (Projects Officer Authorization)
 router.post('/projects/:projectId/request-deletion-otp', authenticateAdmin, requestProjectDeletionOTP);
 router.post('/projects/:projectId/verify-deletion-otp', authenticateAdmin, verifyOTPAndDeleteProject);
