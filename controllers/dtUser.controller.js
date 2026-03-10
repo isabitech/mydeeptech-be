e930e474afcbae3fb9a30aff5c84e632aec49891
Olawumi-Olusegun <olawumi.olusegun@gmail.com>
2026-03-10 08:13:20 +0100

    Update

--- controllers/dtUser.controller.js
+++ controllers/dtUser.controller.js
@@ -2700,7 +2700,7 @@ const adminLogin = async (req, res) => {
 
     console.log("✅ Admin login successful for:", email);
 
-    // 5️⃣ Return success response
+    // Return success response
     res.status(200).json({
       success: true,
       message: "Admin login successful",
